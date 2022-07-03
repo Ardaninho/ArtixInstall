d60f1689e2eb61e5358d6d9162f7d0ef0b2ce149
Ardaninho <103660564+Ardaninho@users.noreply.github.com>
2022-07-03 14:32:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,4 +40,18 @@ echo "Generating fstab"
 fstabgen -U /mnt >> /mnt/etc/fstab
 echo "Done."
 echo "You will be chrooted into your new system."
-artix-chroot /mnt
+echo "Setting up timezone. What is your timezone? ex Europe/Brussels"
+read -r timezone
+echo "Linking $timezone to /etc/localtime..."
+artix-chroot /mnt sh -c "ln -sf /usr/share/zoneinfo/$timezone /etc/localtime"
+echo "hwclock --systohc..."
+artix-chroot /mnt sh -c "hwclock --systohc"
+artix-chroot /mnt sh -c "pacman -S nano"
+echo "Add your locales in /etc/locale.gen. Opening nano in 5 seconds..."
+sleep 5
+artix-chroot /mnt sh -c "nano /etc/locale.gen"
+echo "Installing bootloader... Default bootloader will be GRUB."
+artix-chroot /mnt sh -c "pacman -S grub os-prober efibootmgr:grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id-grub:grub-mkconfig -o /boot/grub/grub.cfg"
+echo "Installation of Artix Linux is done! You can reboot your system now."
+echo "Exiting."
+exit
