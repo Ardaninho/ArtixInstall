acc8fbab58bc6a442a3ce36e396bcfacd681ab4d
Ardaninho <103660564+Ardaninho@users.noreply.github.com>
2022-07-03 14:51:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,6 +55,8 @@ echo "Installing bootloader... Default bootloader will be GRUB."
 artix-chroot /mnt sh -c "pacman -S grub os-prober efibootmgr"
 artix-chroot /mnt sh -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub"
 artix-chroot /mnt sh -c "grub-mkconfig -o /boot/grub/grub.cfg"
+echo "Changing password for root."
+artix-chroot /mnt sh -c "passwd root"
 echo "Installation of Artix Linux is done! You can reboot your system now."
 echo "Exiting."
 exit
